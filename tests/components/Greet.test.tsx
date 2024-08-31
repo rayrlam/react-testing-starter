e945438c51642c3dd7f1ba0a57b7cde84a0beaa5
Ray Lam <raylamedeas@gmail.com>
2024-08-31 21:33:04 +0100

    Simplifying Test Setup

--- tests/components/Greet.test.tsx
+++ tests/components/Greet.test.tsx
@@ -1,7 +1,6 @@
-import { it, expect, describe } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Greet from '../../src/components/Greet';
-import '@testing-library/jest-dom/vitest';
+
 
 describe('Greet', () => {
     it('should render Hellow with the name when name is provided', () => {
