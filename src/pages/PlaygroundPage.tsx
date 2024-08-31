3248ea320ae75a6311a926afec15b688a68328f0
Ray Lam <raylamedeas@gmail.com>
2024-08-31 23:23:17 +0100

    Undo the changes of PlaygroundPage

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,8 +1,7 @@
 import Onboarding from "../components/Onboarding";
-import TermsAndConditions from "../components/TermsAndConditions";
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />;
+  return <Onboarding />;
 };
 
 export default PlaygroundPage;
