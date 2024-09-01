b7f9973ff7abd32275a329a2a6656f804b9fedec
Ray Lam <raylamedeas@gmail.com>
2024-09-01 11:46:18 +0100

    Add test that should expand text when Show More button is clicked

--- tests/components/ExpandableText.test.tsx
+++ tests/components/ExpandableText.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import ExpandableText from '../../src/components/ExpandableText';
+import userEvent from '@testing-library/user-event';
 
 describe('ExpandableText', () => {
     const limit = 255;
@@ -18,5 +19,16 @@ describe('ExpandableText', () => {
 
         const button = screen.getByRole('button');
         expect(button).toHaveTextContent(/more/i);
+    })
+
+    it('should expand text when Show More button is clicked', async () => {
+        render(<ExpandableText text={longText} />);    
+ 
+        const button = screen.getByRole('button');
+        const user = userEvent.setup();
+        await user.click(button);
+
+        expect(screen.getByText(longText)).toBeInTheDocument();
+        expect(button).toHaveTextContent(/less/i);
     })
 })
