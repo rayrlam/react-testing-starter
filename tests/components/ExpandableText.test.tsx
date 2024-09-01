d760e15d8b1d6172487581a41a6f7d1233ffef67
Ray Lam <raylamedeas@gmail.com>
2024-09-01 11:29:25 +0100

    ExpandableText render test if characters longer than 255

--- tests/components/ExpandableText.test.tsx
+++ tests/components/ExpandableText.test.tsx
@@ -9,4 +9,16 @@ describe('ExpandableText', () => {
 
         expect(screen.getByText(text)).toBeInTheDocument();
     })
+
+    it('should truncate text if longer than 255 characters', () => {
+        const text = 'a'.repeat(256);
+
+        render(<ExpandableText text={text} />);    
+
+        const truncatedText = text.substring(0,255) + '...';
+        expect(screen.getByText(truncatedText)).toBeInTheDocument();
+
+        const button = screen.getByRole('button');
+        expect(button).toHaveTextContent(/more/i);
+    })
 })
